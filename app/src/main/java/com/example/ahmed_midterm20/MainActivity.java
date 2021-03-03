8c096cd2bc8f8205f66c64b8bd517e641a976e4b
ahmdiab97 <79974376+ahmdiab97@users.noreply.github.com>
2021-03-03 14:24:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ahmed_midterm20/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ahmed_midterm20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
